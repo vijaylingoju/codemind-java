242083b8a6076315dcebd1abebd8c3eaa6d7a703
Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2022-08-22 01:01:43 +0530

    import java.util.Scanner; class circle {     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         char ch = sc.next().charAt(0);         int a = ch;         System.out.println(a);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class circle
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        int a = ch;
+        System.out.println(a);
+    }
+}
