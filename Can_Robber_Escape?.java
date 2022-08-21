f1664d4968650b021aec12780fa7cbdeb76e1b8e
Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2022-08-22 00:39:52 +0530

    import java.util.Scanner; class circle {     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         int c=0;         int n = sc.nextInt();         int arr[] = new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             if(arr[i]%2!=0)             {                 c++;             }         }         if(c<=2)         {             System.out.println("YES");         }         else         {             System.out.println("NO");         }
        }
    }

---
+++ Can_Robber_Escape?.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class circle
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        int c=0;
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]%2!=0)
+            {
+                c++;
+            }
+        }
+        if(c<=2)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+        
+    }
+}
